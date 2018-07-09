b4d8ab8f71f037ec9bf8ebc563b559ece17168e2
gazprom05 <gazprom05@mail.ru>
2018-07-09 10:13:34 +0300

    Добавлены задания easy и normal

--- Homework_1.py
+++ Homework_1.py
@@ -31,3 +31,45 @@ elif (age > 55) & (age <= 90):
 else:
     print('Ты или труп или ребенок')
 
+# Easy, задача 1
+# a = 10
+# b = 'привет'
+# c = True
+#
+# print(a,b,c)
+# b = input('Введите значение для переменной c')
+# print(a,b,c)
+
+
+# Easy, задача 2
+# d = int(input('Введите любое число'))
+# d = d + 2
+# print(d)
+
+# Easy, задача 3
+# e = int(input('Склдько тебе лет?'))
+# if e >17:
+#     print('Доступ разрешен')
+# else:
+#     print('Извините, доступ с 18 лет')
+
+
+# Normal, задача 1
+
+# while True:
+#     f = int(input('Введите число'))
+#     if (f >0) and (f < 10):
+#         f **= 2
+#         print(f)
+#         break
+#     else:
+#         print('Введите число от 1 до 9')
+
+# Normal, Задача 2
+
+# g = int(input('введите число'))
+# h = int(input('введите другое число'))
+# print(g, h)
+# print('Меняем местами')
+# g,h = h,g
+# print(g,h)
